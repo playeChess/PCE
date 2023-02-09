b922fa0c2f5e1a5a701e2726d61c4b4ad4d55b12
unknown <timeo.perrinjaquet@gmail.com>
2023-02-09 13:44:28 +0100

    Started to add collisions and trying to fix errors with namespaces

--- pce.cpp
+++ pce.cpp
@@ -42,49 +42,36 @@ namespace PlayeChessEngine {
 
             class Piece {
                 protected:
-                    bool is_white;
                     piece_type type;
+                    int coords[2] = {0, 0};
                 public:
-                    Piece(piece_type type, bool is_white) {
+                    bool is_white;
+                    Piece(piece_type type, bool is_white, int x, int y) {
                         this->type = type;
                         this->is_white = is_white;
+                        this->coords[0] = x;
+                        this->coords[1] = y;
                     }
 
-                    virtual bool validation_function(int x_offset, int y_offset) { return false; };
+                    virtual bool validation_function(PlayeChessEngine::board::Board board, int x_offset, int y_offset) { return false; };
+
+                    bool validate_validation(Board board, int x_offset, int y_offset) {
+                        pieces::Piece* pieceptr = board.get_piece(this->coords[0] + x_offset, this->coords[1] + y_offset);
+                        pieces::Piece piece = *pieceptr;
+                        if(!(pieceptr != nullptr || piece.is_white == this->is_white))
+                            return true;
+                        return false;
+                    }
+
+                    bool check_path();
 
                     std::string show() {
                         if (this->is_white) {
-                            switch (this->type) {
-                                case p:
-                                    return "P";
-                                case r:
-                                    return "R";
-                                case n:
-                                    return "N";
-                                case b:
-                                    return "B";
-                                case q:
-                                    return "Q";
-                                case k:
-                                    return "K";
-                            }
-                        } else {
-                            switch (this->type) {
-                                case p:
-                                    return "p";
-                                case r:
-                                    return "r";
-                                case n:
-                                    return "n";
-                                case b:
-                                    return "b";
-                                case q:
-                                    return "q";
-                                case k:
-                                    return "k";
-                            }
+                            std::string piece_names[6] = {"P", "R", "N", "B", "Q", "K"};
+                            return piece_names[this->type];
                         }
-                        return " ";
+                        std::string piece_names[6] = {"p", "r", "n", "b", "q", "k"};
+                        return piece_names[this->type];
                     }
             };
 
@@ -92,21 +79,16 @@ namespace PlayeChessEngine {
                 private:
                     bool has_moved = false;
                 public:
-                    Pawn(bool is_white) : Piece(p, is_white) {};
-                    bool validation_function(int x_offset, int y_offset) {
-                        // std::cout << x_offset << " " << y_offset << std::endl;
+                    Pawn(bool is_white, int x, int y) : Piece(p, is_white, x, y) {};
+                    bool validation_function(PlayeChessEngine::board::Board board, int x_offset, int y_offset) {
                         if(y_offset == 0) {
                             if(this->is_white) {
-                                if (x_offset == 1)
-                                    return true;
-                                if(!this->has_moved && x_offset == 2)
-                                    return true;
+                                if (x_offset == 1 || (!this->has_moved && x_offset == 2))
+                                    return validate_validation(board, x_offset, y_offset);
                                 return false;
                             } else {
-                                if (x_offset == -1)
-                                    return true;
-                                if(!this->has_moved && x_offset == -2)
-                                    return true;
+                                if (x_offset == -1 || (!this->has_moved && x_offset == -2))
+                                    return validate_validation(board, x_offset, y_offset);
                                 return false;
                             }
                         }
@@ -116,7 +98,7 @@ namespace PlayeChessEngine {
 
             class Rook : public Piece {
                 public:
-                    Rook(bool is_white) : Piece(r, is_white) {};
+                    Rook(bool is_white, int x, int y) : Piece(r, is_white, x, y) {};
                     bool validation_function(int x_offset, int y_offset) {
                         if ((x_offset == 0 || y_offset == 0) && x_offset != y_offset)
                             return true;
@@ -126,7 +108,7 @@ namespace PlayeChessEngine {
 
             class Knight : public Piece {
                 public:
-                    Knight(bool is_white) : Piece(n, is_white) {};
+                    Knight(bool is_white, int x, int y) : Piece(n, is_white, x, y) {};
                     bool validation_function(int x_offset, int y_offset) {
                         if ((abs(x_offset) == 2 && abs(y_offset) == 1) || (abs(x_offset) == 1 && abs(y_offset) == 2))
                             return true;
@@ -136,7 +118,7 @@ namespace PlayeChessEngine {
 
             class Bishop : public Piece {
                 public:
-                    Bishop(bool is_white) : Piece(b, is_white) {};
+                    Bishop(bool is_white, int x, int y) : Piece(b, is_white, x, y) {};
                     bool validation_function(int x_offset, int y_offset) {
                         if (abs(x_offset) == abs(y_offset) && x_offset != 0)
                             return true;
@@ -146,7 +128,7 @@ namespace PlayeChessEngine {
 
             class Queen : public Piece {
                 public:
-                    Queen(bool is_white) : Piece(q, is_white) {};
+                    Queen(bool is_white, int x, int y) : Piece(q, is_white, x, y) {};
                     bool validation_function(int x_offset, int y_offset) {
                         if ((abs(x_offset) == abs(y_offset) && x_offset != 0) || ((x_offset == 0 || y_offset == 0) && x_offset != y_offset))
                             return true;
@@ -156,7 +138,7 @@ namespace PlayeChessEngine {
 
             class King : public Piece {
                 public:
-                    King(bool is_white) : Piece(k, is_white) {};
+                    King(bool is_white, int x, int y) : Piece(k, is_white, x, y) {};
                     bool validation_function(int x_offset, int y_offset) {
                         if (abs(x_offset) <= 1 && abs(y_offset) <= 1 && (x_offset != 0 || y_offset != 0))
                             return true;
@@ -168,39 +150,38 @@ namespace PlayeChessEngine {
         class Board {
             private:
                 std::array<std::array<pieces::Piece*, 8>, 8> board = {{{nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}}};
-                pieces::Rook R1 = pieces::Rook(true);
-                pieces::Rook R2 = pieces::Rook(true);
-                pieces::Knight N1 = pieces::Knight(true);
-                pieces::Knight N2 = pieces::Knight(true);
-                pieces::Bishop B1 = pieces::Bishop(true);
-                pieces::Bishop B2 = pieces::Bishop(true);
-                pieces::Queen Q = pieces::Queen(true);
-                pieces::King K = pieces::King(true);
-                pieces::Pawn P1 = pieces::Pawn(true);
-                pieces::Pawn P2 = pieces::Pawn(true);
-                pieces::Pawn P3 = pieces::Pawn(true);
-                pieces::Pawn P4 = pieces::Pawn(true);
-                pieces::Pawn P5 = pieces::Pawn(true);
-                pieces::Pawn P6 = pieces::Pawn(true);
-                pieces::Pawn P7 = pieces::Pawn(true);
-                pieces::Pawn P8 = pieces::Pawn(true);
-
-                pieces::Rook r1 = pieces::Rook(false);
-                pieces::Rook r2 = pieces::Rook(false);
-                pieces::Knight n1 = pieces::Knight(false);
-                pieces::Knight n2 = pieces::Knight(false);
-                pieces::Bishop b1 = pieces::Bishop(false);
-                pieces::Bishop b2 = pieces::Bishop(false);
-                pieces::Queen q = pieces::Queen(false);
-                pieces::King k = pieces::King(false);
-                pieces::Pawn p1 = pieces::Pawn(false);
-                pieces::Pawn p2 = pieces::Pawn(false);
-                pieces::Pawn p3 = pieces::Pawn(false);
-                pieces::Pawn p4 = pieces::Pawn(false);
-                pieces::Pawn p5 = pieces::Pawn(false);
-                pieces::Pawn p6 = pieces::Pawn(false);
-                pieces::Pawn p7 = pieces::Pawn(false);
-                pieces::Pawn p8 = pieces::Pawn(false);
+                pieces::Rook R1 = pieces::Rook(true, 0, 0);
+                pieces::Rook R2 = pieces::Rook(true, 0, 7);
+                pieces::Knight N1 = pieces::Knight(true, 0, 1);
+                pieces::Knight N2 = pieces::Knight(true, 0, 6);
+                pieces::Bishop B1 = pieces::Bishop(true, 0, 2);
+                pieces::Bishop B2 = pieces::Bishop(true, 0, 5);
+                pieces::Queen Q = pieces::Queen(true, 0, 3);
+                pieces::King K = pieces::King(true, 0, 4);
+                pieces::Pawn P1 = pieces::Pawn(true, 1, 0);
+                pieces::Pawn P2 = pieces::Pawn(true, 1, 1);
+                pieces::Pawn P3 = pieces::Pawn(true, 1, 2);
+                pieces::Pawn P4 = pieces::Pawn(true, 1, 3);
+                pieces::Pawn P5 = pieces::Pawn(true, 1, 4);
+                pieces::Pawn P6 = pieces::Pawn(true, 1, 5);
+                pieces::Pawn P7 = pieces::Pawn(true, 1, 6);
+                pieces::Pawn P8 = pieces::Pawn(true, 1, 7);
+                pieces::Rook r1 = pieces::Rook(false, 7, 0);
+                pieces::Rook r2 = pieces::Rook(false, 7, 7);
+                pieces::Knight n1 = pieces::Knight(false, 7, 1);
+                pieces::Knight n2 = pieces::Knight(false, 7, 6);
+                pieces::Bishop b1 = pieces::Bishop(false, 7, 2);
+                pieces::Bishop b2 = pieces::Bishop(false, 7, 5);
+                pieces::Queen q = pieces::Queen(false, 7, 3);
+                pieces::King k = pieces::King(false, 7, 4);
+                pieces::Pawn p1 = pieces::Pawn(false, 6, 0);
+                pieces::Pawn p2 = pieces::Pawn(false, 6, 1);
+                pieces::Pawn p3 = pieces::Pawn(false, 6, 2);
+                pieces::Pawn p4 = pieces::Pawn(false, 6, 3);
+                pieces::Pawn p5 = pieces::Pawn(false, 6, 4);
+                pieces::Pawn p6 = pieces::Pawn(false, 6, 5);
+                pieces::Pawn p7 = pieces::Pawn(false, 6, 6);
+                pieces::Pawn p8 = pieces::Pawn(false, 6, 7);
 
             public:
                 Board() {
@@ -265,13 +246,17 @@ namespace PlayeChessEngine {
                     std::cout <<  "    a b c d e f g h" << std::endl;
                 }
 
+                pieces::Piece* get_piece(int x, int y) {
+                    return board[x][y];
+                };
+
                 void show_moves(int x, int y) {
                     std::vector<PlayeChessEngine::Move> moves;
                     std::cout << "Piece: " << board[x][y]->show() << std::endl;
                     for(int i = 0; i < 8; i++) {
                         for(int j = 0; j < 8; j++) {
                             // std::cout << "Checking: " << i << " " << j << std::endl;
-                            if(board[x][y]->validation_function(i - x, j - y)) {
+                            if(board[x][y]->validation_function(board, i - x, j - y)) {
                                 PlayeChessEngine::Move(x, y, i, j).show();
                                 // std::cout << "Move: " << x << " " << y << " " << i << " " << j << std::endl;
                             }
@@ -283,7 +268,7 @@ namespace PlayeChessEngine {
                     std::vector<PlayeChessEngine::Move> moves;
                     for(int i = 0; i < 8; i++) {
                         for(int j = 0; j < 8; j++) {
-                            if(board[x][y]->validation_function(i - x, j - y)) {
+                            if(board[x][y]->validation_function(board, i - x, j - y)) {
                                 moves.push_back(PlayeChessEngine::Move(x, y, i, j));
                             }
                         }
